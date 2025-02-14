b579d44ca8f95fb9da1cb92c910dd2a9edc57994
dev  intern <it.dcom2023@gmail.com>
2025-02-14 10:37:56 +0000

    Edit table.go

--- Entity/table.go
+++ Entity/table.go
@@ -56,7 +56,7 @@ type CancelStatus struct {
 	CancelID     int       `db:"CancelID"`     // รหัสการยกเลิก (PK - Auto Increment)
 	RefID        string    `db:"RefID"`        // เลขที่ใบสั่งซื้อ (FK -> RecID(BeforeReturnOrder) || RuturnID(ReturnOrder))
 	SourceTable  string    `db:"SourceTable"`  // ตารางที่ส่งข้อมูล (BeforeReturnOrder || ReturnOrder)
-	CancelReason string    `db:"CancelReason"` // เหตุผลการยกเลิกคคืนสินค้า
+	CancelReason string    `db:"CancelReason"` // เหตุผลการยกเลิกคืนสินค้า
 	CancelBy     string    `db:"CancelBy"`     // ผู้ยกเลิก
 	CancelDate   time.Time `db:"CancelDate"`   // วันที่ยกเลิก
 }
