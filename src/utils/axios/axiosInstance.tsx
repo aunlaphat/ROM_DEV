a08e8c03d920e3e8f0ee93c6b0dc6bf5e682ae9f
Aunlaphat Artsuriyong <aunlaphat.art@dcapp.pro>
2025-03-07 14:44:26 +0700

    [update] [Import Order to WareHouse] Sale_Return search/get orderNo, get sku for photo by order, get orderLine by order summary

--- src/utils/axios/axiosInstance.tsx
+++ src/utils/axios/axiosInstance.tsx
@@ -8,4 +8,17 @@ const api = axios.create({
   },
 });
 
+
+// เพิ่ม Interceptors เพื่อจัดการ Token หรือ Error Handling ได้
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token"); // ตัวอย่างการดึง token
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
 export default api;
